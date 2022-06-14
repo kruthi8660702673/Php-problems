6dce86e07250f31dc93f169eeb9fa7ffe5a85eb3
Kruthika <kruthikagnayaka05@gmail.com>
2022-06-14 09:10:59 +0530

    Functional Problems

--- Functional-Problems/Windchill.php
+++ Functional-Problems/Windchill.php
@@ -1,13 +1,13 @@
 <?php
 
-$t = readline('Enter the value of t: ');
+$t = readline('Enter the value of t: ');            //user input
 $v = readline('Enter the value of v: ');
 
 function wind($t, $v){
-    $w = (35.74 +0.6215*$t +(0.4275*$t - 35.75)*pow($v,0.16));
-    echo $w;
+    $w = (35.74 +0.6215*$t +(0.4275*$t - 35.75)*pow($v,0.16));          //formula for windchill
+    echo $w;                                        //printing the output
 
 }
 
-wind($t,$v);
+wind($t,$v);                        
 ?>
